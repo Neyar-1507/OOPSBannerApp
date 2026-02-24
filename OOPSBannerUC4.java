eb6764b3d478cb69c45183a9cd3fc457911b231c
Neyar-1507 <neyarnishanth15@gmail.com>
2026-02-24 13:10:42 +0530

    Errors fixed

--- OOPSBannerUC4.java
+++ OOPSBannerUC4.java
@@ -1,15 +1,16 @@
 public class OOPSBannerUC4{
 	public static void main(String[] args){
-	lines[0]=System.out.println(String.join(" ","   ***   ","   ***   " , "******  " , " *****  "));
-	lines[1]=System.out.println(String.join(" "," **   ** "," **   ** " , "**   ** " , "**   ** "));
-	lines[2]=System.out.println(String.join(" ","**     **","**     **" , "**    **" , "**      "));
-	lines[3]=System.out.println(String.join(" ","**     **","**     **" , "**   ** " , " *****  "));
-	lines[4]=System.out.println(String.join(" ","**     **","**     **" , "******  " , "     ** "));
-	lines[5]=System.out.println(String.join(" "," **   ** "," **   ** " , "**      " , "**   ** "));
-	lines[6]=System.out.println(String.join(" ","   ***   ","   ***   " , "**      " , " *****  "));
-	}
+		String[] lines=new String[7];
+	lines[0]=String.join(" ","   ***   ","   ***   " , "******  " , " *****  ");
+	lines[1]=String.join(" "," **   ** "," **   ** " , "**   ** " , "**   ** ");
+	lines[2]=String.join(" ","**     **","**     **" , "**    **" , "**      ");
+	lines[3]=String.join(" ","**     **","**     **" , "**   ** " , " *****  ");
+	lines[4]=String.join(" ","**     **","**     **" , "******  " , "     ** ");
+	lines[5]=String.join(" "," **   ** "," **   ** " , "**      " , "**   ** ");
+	lines[6]=String.join(" ","   ***   ","   ***   " , "**      " , " *****  ");
+	
 	for(String line : lines){
 		System.out.println(line);
 	}
-	
+  }
 }
